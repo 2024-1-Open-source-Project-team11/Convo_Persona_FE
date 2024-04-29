17083d02d6d53120e3d40b40b096141acbd02bb7
selfishAltruism <officialkyus@gmail.com>
2024-04-29 10:27:55 +0900

    feat: create chat dto

--- src/@types/chat.d.ts
+++ src/@types/chat.d.ts
@@ -1,7 +1,17 @@
 declare namespace Chat {
   //DTO
+  export interface Message {
+    id: string;
+    sender: "USER" | "GPT";
+    content: stirng;
+  }
+
   export interface AllChatResDto {
-    name: string;
-    password: string;
+    id: string;
+    message: Message[];
+  }
+
+  export interface AddChatReqDto {
+    content: stirng;
   }
 }
