5432d857396e6b0e2df43a43dea51fc597e9d8ee
selfishAltruism <officialkyus@gmail.com>
2024-04-29 10:19:10 +0900

    feat: add chat file (type, service)

---
+++ src/@types/chat.d.ts
@@ -0,0 +1,7 @@
+declare namespace Chat {
+  //DTO
+  export interface AllChatResDto {
+    name: string;
+    password: string;
+  }
+}
---
+++ src/service/ChatService.tsx
@@ -0,0 +1,31 @@
+import { AxiosResponse } from "axios";
+
+import { API } from "@/config/axios";
+
+const ChatService = () => {
+  const URL = "api/v1/chat";
+
+  //All chat load
+
+  //New chat add (res is chat respense)
+
+  //Chat reset
+
+  //Info reset
+
+  /* const signin = async (body: User.SignInReqDto) => {
+    const {
+      data: { accessToken, refreshToken },
+    } = (await API.post(
+      `${URL}/sign-in`,
+      body
+    )) as AxiosResponse<User.SignInResDto>;
+
+    setAccess(accessToken);
+    storeRefresh(refreshToken);
+  }; */
+
+  return [];
+};
+
+export default ChatService;
