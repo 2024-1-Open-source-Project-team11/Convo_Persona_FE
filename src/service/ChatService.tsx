fb4d5960fe0aef631a24a2bd2897348f422e2729
selfishAltruism <officialkyus@gmail.com>
2024-05-06 21:56:52 +0900

    feat: complite chat service

--- src/service/ChatService.tsx
+++ src/service/ChatService.tsx
@@ -2,37 +2,36 @@ import { AxiosResponse } from "axios";
 
 import { API } from "@/config/axios";
 
+import useChatState from "@/store/chatStore";
+
 const ChatService = () => {
   const URL = "api/v1/chat";
 
+  const setChat = useChatState((state) => state.setChat);
+  const addResiveMessage = useChatState((state) => state.addResiveMessage);
+
   //All chat load
   const loadAllChat = async () => {
-    const {
-      data: { message },
-    } = (await API.get(`${URL}`)) as AxiosResponse<Chat.Chat>;
+    const { data } = (await API.get(`${URL}`)) as AxiosResponse<Chat.Chat>;
 
-    console.log(message);
+    setChat(data);
   };
 
   //New chat add (res is chat respense)
+  const postUserMessage = async (body: Chat.AddChatReqDto) => {
+    const { data } = (await API.post(
+      `${URL}`,
+      body
+    )) as AxiosResponse<Chat.AddChatResDto>;
+
+    addResiveMessage(data);
+  };
 
   //Chat reset
 
   //Info reset
 
-  /* const signin = async (body: User.SignInReqDto) => {
-    const {
-      data: { accessToken, refreshToken },
-    } = (await API.post(
-      `${URL}/sign-in`,
-      body
-    )) as AxiosResponse<User.SignInResDto>;
-
-    setAccess(accessToken);
-    storeRefresh(refreshToken);
-  }; */
-
-  return [loadAllChat];
+  return [loadAllChat, postUserMessage];
 };
 
 export default ChatService;
