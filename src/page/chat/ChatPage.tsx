199f725375aaf35c2ded6e5e475800f1f0bc982b
selfishAltruism <officialkyus@gmail.com>
2024-05-17 22:03:28 +0900

    feat: separation styles chat page, add mbti loading system

--- src/page/chat/ChatPage.tsx
+++ src/page/chat/ChatPage.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-import styled from "@emotion/styled";
+
 import { useForm, SubmitHandler } from "react-hook-form";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import Avatar from "@mui/material/Avatar";
-import RefreshIcon from "@mui/icons-material/Refresh";
 
 import { useNavigate } from "react-router-dom";
 
@@ -24,6 +22,8 @@ import {
 } from "@/component/Container";
 import Logo from "@/component/Logo";
 import Loading from "@/component/GptLoading";
+
+import * as Styles from "./ChatPageStyles";
 
 const ChatPage = () => {
   const { register, handleSubmit, setValue } = useForm<Chat.AddChatReqDto>();
@@ -61,12 +61,18 @@ const ChatPage = () => {
       <PinkBackground />
       <Container>
         <Logo type="SMALL" />
-        <Img src="/img/signin_logo.png" alt="logo" />
-        <MbtiContainer>
-          <MbtiSubText>User's MBTI</MbtiSubText>
-          <MbtiMainText>ENTJ</MbtiMainText>
-        </MbtiContainer>
-        <RefreshButton
+        <Styles.Img src="/img/signin_logo.png" alt="logo" />
+        <Styles.MbtiContainer>
+          <Styles.MbtiSubText>User's MBTI</Styles.MbtiSubText>
+          <Styles.MbtiMainText>
+            {chat.message[0] && chat.message[0].mbti != "UNDEFINED" ? (
+              chat.message[0].mbti
+            ) : (
+              <Loading />
+            )}
+          </Styles.MbtiMainText>
+        </Styles.MbtiContainer>
+        <Styles.RefreshButton
           onClick={() => {
             refreshInfo();
           }}
@@ -74,16 +80,16 @@ const ChatPage = () => {
         <ChatContainer>
           {standby ? (
             <GptMessageContainer>
-              <ProfileImg
+              <Styles.ProfileImg
                 alt="Gpt Profile Img"
                 src="img/gptImg.png"
                 sx={{ width: 40, height: 40 }}
               />
               <MessageContantContainer>
-                <ProfileText>Covon persona</ProfileText>
-                <Content style={{ height: "60px" }}>
+                <Styles.ProfileText>Covon persona</Styles.ProfileText>
+                <Styles.Content style={{ height: "60px" }}>
                   <Loading />
-                </Content>
+                </Styles.Content>
               </MessageContantContainer>
             </GptMessageContainer>
           ) : null}
@@ -91,26 +97,26 @@ const ChatPage = () => {
             <>
               {message.sender === "USER" ? (
                 <UserMessageContainer>
-                  <ProfileImg
+                  <Styles.ProfileImg
                     alt="User Profile Img"
                     src="img/userImg.png"
                     sx={{ width: 40, height: 40 }}
                   />
                   <MessageContantContainer>
-                    <ProfileText>User</ProfileText>
-                    <Content>{message.content}</Content>
+                    <Styles.ProfileText>User</Styles.ProfileText>
+                    <Styles.Content>{message.content}</Styles.Content>
                   </MessageContantContainer>
                 </UserMessageContainer>
               ) : (
                 <GptMessageContainer>
-                  <ProfileImg
+                  <Styles.ProfileImg
                     alt="Gpt Profile Img"
                     src="img/gptImg.png"
                     sx={{ width: 40, height: 40 }}
                   />
                   <MessageContantContainer>
-                    <ProfileText>Convo Persona</ProfileText>
-                    <Content>{message.content}</Content>
+                    <Styles.ProfileText>Convo Persona</Styles.ProfileText>
+                    <Styles.Content>{message.content}</Styles.Content>
                   </MessageContantContainer>
                 </GptMessageContainer>
               )}
@@ -118,151 +124,20 @@ const ChatPage = () => {
           ))}
         </ChatContainer>
         <ChatInputContainer onSubmit={handleSubmit(onSubmit)}>
-          <Input
+          <Styles.Input
             placeholder="Message to Convo Persona..."
             {...register("content", { required: "message를 입력해주세요!" })}
           />
-          <SubmitButton disabled={standby} onClick={handleSubmit(onSubmit)}>
+          <Styles.SubmitButton
+            disabled={standby}
+            onClick={handleSubmit(onSubmit)}
+          >
             <ArrowUpwardIcon />
-          </SubmitButton>
+          </Styles.SubmitButton>
         </ChatInputContainer>
       </Container>
     </>
   );
 };
-
-const Input = styled.input`
-  background-color: white;
-
-  width: 266px;
-  height: 40px;
-
-  border: 0px white solid;
-  border-left: 5px white solid;
-  border-radius: 7px;
-
-  outline: none;
-
-  font-size: 15px;
-`;
-
-const SubmitButton = styled.button`
-  background-color: #ff4646;
-
-  width: 57px;
-  height: 42px;
-
-  border: 0px white solid;
-  border-radius: 7px;
-
-  box-shadow: 0px 5px 0 -0.5px black;
-
-  margin-top: -3px;
-  margin-bottom: 5px;
-
-  color: white;
-
-  :hover {
-    box-shadow: 0 0 0 0 black;
-    margin-bottom: -5px;
-  }
-`;
-
-const RefreshButton = styled(RefreshIcon)`
-  position: absolute;
-  top: 14px;
-  right: -7px;
-
-  font-size: 45px;
-  color: white;
-`;
-
-const ProfileImg = styled(Avatar)`
-  top: 5px;
-  left: 5px;
-
-  border: 2px solid #ff7a85;
-
-  margin-right: 10px;
-  margin-bottom: 9px;
-`;
-
-const ProfileText = styled.div`
-  color: inherit;
-
-  font-size: 15px;
-  font-weight: bold;
-
-  margin-top: 3px;
-`;
-
-const Content = styled.div`
-  width: max-content;
-  width: 98%;
-  word-break: break-all;
-
-  margin-bottom: 5px;
-
-  color: inherit;
-  font-size: 13px;
-`;
-
-const MbtiContainer = styled.div`
-  position: absolute;
-
-  border: 5px white solid;
-  border-radius: 10px;
-
-  width: 130px;
-  height: 130px;
-
-  left: -200px;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const MbtiMainText = styled.div`
-  color: inherit;
-
-  font-size: 46px;
-  font-weight: bold;
-
-  margin-top: 3px;
-
-  color: white;
-
-  margin-top: 10px;
-  margin-bottom: 20px;
-`;
-
-const MbtiSubText = styled.div`
-  background-color: white;
-  width: 100%;
-  height: 30%;
-
-  color: #ff7a85;
-
-  border: 3px white solid;
-
-  font-size: 18px;
-  font-weight: bold;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Img = styled.img`
-  position: absolute;
-
-  top: 171px;
-  left: -230px;
-  width: 150px;
-
-  z-index: 3;
-`;
 
 export default ChatPage;
